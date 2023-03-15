b41df030788b542cc6184d1e7cf0627caa0b8c4e
V.tsutsurin <v.tsutsurin@yandex.ru>
2023-03-15 23:03:07 +0300

    added discount

--- salon/discount/models.py
+++ salon/discount/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Discount(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='discount/images/')
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- salon/discount/views.py
+++ salon/discount/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Discount
+
 
 def index(request):
-    return render(request, 'discount/index.html')
+    discount = Discount.objects.order_by('-date')
+    return render(request, 'discount/index.html', {'discounts': discount})
+
+
+def detail(request, discount_id):
+    discount = get_object_or_404(Discount, pk=discount_id)
+    return render(request, 'discount/discoubt_page.html', {'discount': discount})
