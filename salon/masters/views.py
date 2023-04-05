bc86697a0efa4f743b7d7d4543007a93840bdfda
V.tsutsurin <v.tsutsurin@yandex.ru>
2023-04-05 22:56:53 +0300

    completed masters

--- salon/masters/views.py
+++ salon/masters/views.py
@@ -4,4 +4,11 @@ from .models import Master
 
 def masters(request):
     master = Master.objects.order_by('name')
-    return render(request, 'masters/masters.html', {'masters': master})
+    master1 = Master.objects.filter(location='1')
+    master2 = Master.objects.filter(location='2')
+    masters_all = {
+        'masters': master,
+        'master1': master1,
+        'master2': master2
+    }
+    return render(request, 'masters/masters.html', masters_all)
